b5180bb0dd8a83b2c661e02846d1633d2a22994f
Pierre-Antoine Guillaume <pierreantoine.guillaume@gmail.com>
2020-02-09 18:06:59 +0100

    :tada: project initialization

---
+++ src/hello.cpp
@@ -0,0 +1,15 @@
+
+
+#include <iostream>
+
+
+using std::cout;
+using std::cerr;
+using std::endl;
+
+int main(int argc, char **argv)
+{
+    cout << "Hello javascript" << endl;
+    cerr << "Error output" << endl;
+    return 0;
+}
